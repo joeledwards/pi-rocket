a7f22a43f04f8c4df54bb4f640bd416f6449aaa4
Joel Edwards <joeledwards@gmail.com>
2016-08-27 14:56:38 +0000

    Corrected config location from droid server.

--- droid/droid.js
+++ droid/droid.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bindPort = 8080;
 const app = express();
 
-let {pubKey, subKey, secret} = JSON.parse(fs.readFileSync('./pubnub.json'));
+let {pubKey, subKey, secret} = JSON.parse(fs.readFileSync('../pubnub.json'));
 const pubnubConfig = {
   ssl: true,
   publish_key: pubKey,
