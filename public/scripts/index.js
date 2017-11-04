1636613d594c8862722502a0fedfcce4f7cb4800
Joel Edwards <joeledwards@gmail.com>
2017-11-03 22:42:13 -0600

    Added clearing of auto-clear events

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -15,23 +15,35 @@ const ignition = (state) => {
 
 // Turn ignition on
 const ignitionOn = () => {
-  console.log('Ignition on.');
-
+  console.log('Ignition on.', JSON.stringify(autoClears));
   return ignition('/on');
 };
 
 // Turn ignition off
 const ignitionOff = () => {
-  console.log('Ignition off.');
-
+  console.log('Ignition off.', JSON.stringify(autoClears));
+  flushAutoClears();
   return ignition('/off');
 };
+
+const flushAutoClears = () => {
+  autoClears.forEach(ac => ac.cancel = true);
+  autoClears = [];
+}
+
+let autoClears = [];
 
 // Launch the rocket (ignition on for 5 seconds, then off)
 const launch = () => {
   ignitionOn()
   .then(() => {
-    setTimeout(ignitionOff, 5000);
+    let autoClear = {id: new Date().getTime(), cancel: false};
+    autoClears.push(autoClear);
+    setTimeout(() => {
+      if (autoClear.cancel !== true) {
+        ignitionOff();
+      }
+    }, 5000)
   })
   .catch((error) => {
     console.error('Ignition error:', error);
