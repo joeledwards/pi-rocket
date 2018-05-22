725d066715609e2fe4b8685bccad9d4a45467a0c
Joel Edwards <joeledwards@gmail.com>
2018-05-21 22:02:28 -0600

    Added logging for unhandled errors

--- server.js
+++ server.js
@@ -394,3 +394,11 @@ function main() {
 
 main();
 
+process
+.on('unhandledRejection', (reason, p) => {
+  console.error(reason, 'Unhandled Rejection at Promise', p);
+})
+.on('uncaughtException', err => {
+  console.error(err, 'Uncaught Exception thrown');
+  process.exit(1);
+});
